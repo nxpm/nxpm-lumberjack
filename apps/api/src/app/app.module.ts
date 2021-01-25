56b528c4ea7feaae1c460d60686b7a0664f79b87
Bram Borggreve <borggreve@gmail.com>
2021-01-25 16:43:58 -0500

    feat: make API serve frontend

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,9 +1,30 @@
-import { Module } from '@nestjs/common'
+import { Logger, Module } from '@nestjs/common'
+import { ServeStaticModule } from '@nestjs/serve-static'
 import { ApiAuthFeatureModule } from '@nxpm-lumberjack/api/auth/feature'
 import { ApiCoreFeatureModule } from '@nxpm-lumberjack/api/core/feature'
 import { ApiLogFeatureModule } from '@nxpm-lumberjack/api/log/feature'
+import { ensureDirSync, existsSync, writeFileSync } from 'fs-extra'
+import { join } from 'path'
+
+const rootPath = join(__dirname, '..', 'web')
 
 @Module({
-  imports: [ApiAuthFeatureModule, ApiCoreFeatureModule, ApiLogFeatureModule],
+  imports: [
+    ApiAuthFeatureModule,
+    ApiCoreFeatureModule,
+    ApiLogFeatureModule,
+    ServeStaticModule.forRoot({
+      rootPath,
+      exclude: ['/api/*', '/graphql'],
+    }),
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    if (!existsSync(rootPath)) {
+      ensureDirSync(rootPath)
+      writeFileSync(join(rootPath, 'index.html'), `<pre>OK</pre>`)
+      Logger.verbose(`Created static root path ${rootPath}`)
+    }
+  }
+}
